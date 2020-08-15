4d198eef746739995d24f42c3cb02d534e120f7f
Hyung97 <tammyhyunglee@gmail.com>
2020-08-14 20:16:07 -0600

    Creacion de la Activity transferencia de datos

---
+++ app/src/main/java/steph/rs/conversiones/TransferenciaDeDatos.java
@@ -0,0 +1,14 @@
+package steph.rs.conversiones;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TransferenciaDeDatos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_transferencia_de_datos);
+    }
+}
